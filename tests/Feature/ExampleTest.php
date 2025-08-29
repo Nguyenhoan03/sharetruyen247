95fff723ae221e5d759d28299cdddafecc60d4a0
Nguyenhoan03 <phephehoan@gmail.com>
2025-08-29 22:23:53 +0700

    test1
    test 2
    
    test 3

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response()
     {
+        dd("test 2");
         $response = $this->get('/');
 
         $response->assertStatus(200);
